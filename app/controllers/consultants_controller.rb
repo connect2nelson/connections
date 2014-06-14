8f54d27b86553ea082c3dfbd4bc197c1676db40d
Peiying Wen <wenpeiying@gmail.com>
2014-06-14 16:25:02 -0700

    redirect to consultant page after select

--- app/controllers/consultants_controller.rb
+++ app/controllers/consultants_controller.rb
@@ -2,7 +2,7 @@ class ConsultantsController < ApplicationController
   before_action :authenticate_user
 
   include Rails3JQueryAutocomplete::Orm::Mongoid
-  autocomplete :consultant, :full_name
+  autocomplete :consultant, :full_name, :full => true, :extra_data => [:employee_id]
 
   def show
     @consultant = Consultant.find_by(employee_id: params[:id])
