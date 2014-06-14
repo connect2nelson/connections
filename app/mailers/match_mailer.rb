68dc9c74edf931d4680b90239c395492d00247a1
Derek Hammer <derek.r.hammer@gmail.com>
2014-06-13 21:43:26 -0700

    Match mailer first iteration

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -1,8 +1,8 @@
-
 class MatchMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@thoughtworks.com"
+  default subject: '[Connections] New connection for you!'
 
   def create_match_message(from, to, time) 
-
+    mail(to: [to, from])
   end
 end
--- spec/mailers/match_mailer_spec.rb
+++ spec/mailers/match_mailer_spec.rb
@@ -1,9 +1,26 @@
-
 require "rails_helper"
 
 RSpec.describe MatchMailer, :type => :mailer do
+  let(:mentor) { 'mentor@thoughtworks.com' }
+  let(:thoughtworker) { 'thoughtworker@thoughtworks.com' }
+  before {
+    MatchMailer.create_match_message(thoughtworker, mentor, Time.now).deliver
+  }
+  subject { ActionMailer::Base.deliveries.first }
+
+  it 'send an email' do
+    expect(subject).to_not be_nil
+  end
   it 'send an email to a mentor' do
-    email = MatchMailer.create_match_message('cupid@thoughtworks.com', 'thoughtworker@thoughtworks.com', Time.now).deliver
-    expect{ActionMailer::Base.deliveries}.empty be_false
+    expect(subject.to).to include mentor
+  end
+  it 'send an email to a thoughtworker' do
+    expect(subject.to).to include thoughtworker
+  end
+  it 'send an email from noreply' do
+    expect(subject.from).to include 'no-reply@thoughtworks.com'
+  end
+  it 'send an email with subject [Connnections] New connection for you!' do
+    expect(subject.subject).to eq '[Connections] New connection for you!'
   end
 end
