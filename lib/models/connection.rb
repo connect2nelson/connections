b766a4173465ff9e22a750522625b10e37221c90
Derek Hammer <derek.r.hammer@gmail.com>
2014-06-13 21:49:34 -0700

    Refactor

--- lib/models/connection.rb
+++ lib/models/connection.rb
@@ -1,7 +1,6 @@
 class Connection
   attr_reader :mentor
   attr_reader :mentee
-  attr_accessor :skill_gap
 
   def initialize(mentor, mentee)
     @mentor = mentor
@@ -10,24 +9,24 @@ class Connection
   end
 
   def match?
-  	has_skill_to_learn? && same_working_office?
+    has_skill_to_learn? && same_working_office?
   end
 
   def same_working_office?
-  	mentor.working_office == mentee.working_office 
+    mentor.working_office == mentee.working_office 
   end
 
   def has_skill_to_learn?
-  	mentor.skills.any? { |skill, level| can_learn_from?(skill, level)} 
+    mentor.skills.any? { |skill, level| can_learn_from?(skill, level)} 
   end
 
   def can_learn_from?(skill, level)
-  	 @skill_gap = 0
-  	 mentees_skill_level = mentee.skills[skill]
-  	 if mentees_skill_level != nil
-  	   @skill_gap += level.to_i - mentees_skill_level.to_i
-  	 end
-  	 mentee.skills[skill].to_i == 1 && level.to_i == 5
+    mentee.skills[skill].to_i == 1 && level.to_i == 5
   end
 
+  def skill_gap
+    mentor.skills.inject(0) do |sum, (skill, level)|
+      sum += level.to_i - mentee.skills[skill].to_i
+    end
+  end
 end
