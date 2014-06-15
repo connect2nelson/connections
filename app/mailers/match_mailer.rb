874e5cd663158e6e0b75e03084108e4cb0541e46
Pamela Ocampo <pamela.ocampo@gmail.com>
2014-06-15 15:41:58 -0700

    Refactored unused mailer methods

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -2,10 +2,6 @@ class MatchMailer < ActionMailer::Base
   default from: "no-reply@thoughtworks.com"
   default subject: '[Connections] New connection for you!'
 
-  def create_match_message(from, to)
-      mail(to: [to, from])
-  end
-
   def send_connections(consultant, connections)
       @consultant = consultant
       @connections = connections
--- spec/mailers/match_mailer_spec.rb
+++ spec/mailers/match_mailer_spec.rb
@@ -1,10 +1,7 @@
 require "rails_helper"
 
 RSpec.describe MatchMailer, :type => :mailer do
-  let(:mentor) { 'mentor@thoughtworks.com' }
-  let(:thoughtworker) { 'thoughtworker@thoughtworks.com' }
   before {
-    mailer = MatchMailer.create_match_message(thoughtworker, mentor).deliver
     @consultant = Consultant.new(employee_id: "15230",
                                  full_name: "Pam Ocampo",
                                  working_office: "San Francisco",
@@ -18,24 +15,19 @@ RSpec.describe MatchMailer, :type => :mailer do
   subject { ActionMailer::Base.deliveries.first }
 
   it 'should send an email' do
-    expect(subject).to_not be_nil
+    expect(@connections_mailer).to_not be_nil
   end
-  it 'should send an email to a mentor' do
-    expect(subject.to).to include mentor
-  end
+
   it 'should send an email to a thoughtworker' do
-    expect(subject.to).to include thoughtworker
+    expect(@connections_mailer.to).to include @consultant.email
   end
+
   it 'should send an email from noreply' do
-    expect(subject.from).to include 'no-reply@thoughtworks.com'
+    expect(@connections_mailer.from).to include 'no-reply@thoughtworks.com'
   end
 
   it 'should send an email with subject [Connnections] New connection for you!' do
-    expect(subject.subject).to eq '[Connections] New connection for you!'
-  end
-
-  it 'should assign a consultant employee email to the mailer' do
-      expect(@connections_mailer.to).to include "#{@consultant.employee_id}@thoughtworks.com"
+    expect(@connections_mailer.subject).to eq '[Connections] New connection for you!'
   end
 
   it 'should have a list of of connection mentors' do
