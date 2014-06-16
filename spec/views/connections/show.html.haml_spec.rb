0fb4d3372695cd3d8c2a2512e084816ae66e9a91
Peiying Wen <wenpeiying@gmail.com>
2014-06-16 15:38:06 -0700

    connections page: adding two consultants info

--- spec/views/connections/show.html.haml_spec.rb
+++ spec/views/connections/show.html.haml_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe "connections/show.html.haml" do
 
   before do
-    assign :consultant_one, Consultant.new(full_name: 'Bob')
-    assign :consultant_two, Consultant.new(full_name: 'Master')
+    assign :consultant_one, Consultant.new(full_name: 'Bob', primary_role: 'Dev')
+    assign :consultant_two, Consultant.new(full_name: 'Master', primary_role: 'QA')
     render
   end
 
   it 'show two consultants' do
     expect(rendered).to have_text('Bob')
+    expect(rendered).to have_text('Dev')
     expect(rendered).to have_text('Master')
+    expect(rendered).to have_text('QA')
   end
 
 end
