280f31414a084d528f2fbe6283617b21088f4afb
Peiying Wen <wenpeiying@gmail.com>
2014-06-16 18:15:27 -0700

    adding tests; removed unused helper and pending tests

--- spec/views/connections/show.html.haml_spec.rb
+++ spec/views/connections/show.html.haml_spec.rb
@@ -7,9 +7,17 @@ RSpec.describe "connections/show.html.haml" do
     consultant_two = Consultant.new(full_name: 'Master', primary_role: 'QA', skills: {})
     connection = Connection.new(consultant_one, consultant_two)
     allow(connection).to receive(:score).and_return(1.5)
+
+    intersecting_skill = SkillDiff.new('Java', consultant_one, consultant_two)
+    intersecting_skills = [intersecting_skill]
+    allow(connection).to receive(:intersecting_skills).and_return(intersecting_skills)
+    allow(intersecting_skill).to receive(:consultant_one_level)
+    allow(intersecting_skill).to receive(:consultant_two_level)
+
     assign :consultant_one, consultant_one
     assign :consultant_two, consultant_two
     assign :connection, connection
+
     render
   end
 
@@ -22,6 +30,10 @@ RSpec.describe "connections/show.html.haml" do
 
   it 'should show score' do
     expect(rendered).to have_text('1.5')
+  end
+
+  it 'should show intersecting skills' do
+    expect(rendered).to have_text('Java')
   end
 
 end
