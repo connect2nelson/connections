b7a71c3cda41250876832535fd82856f1f05c0f0
ThoughtWorker <foo@foo.com>
2014-06-26 15:56:11 -0700

    [Ian] adding default host for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Connections::Application.configure do
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
-
+  config.action_mailer.asset_host = "http://connections-thoughtworks.herokuapp.com/"
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 end
