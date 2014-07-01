db6083ab0ad7e1f12e69bb549897381ec0b831b9
Peiying Wen <wenpeiying@gmail.com>
2014-06-30 22:54:28 -0700

    refactored activity service

--- lib/models/activity_service.rb
+++ lib/models/activity_service.rb
@@ -1,39 +1,41 @@
 class ActivityService
 
-  def self.update_github consultants
-    consultants.each do |consultant|
-      GithubClient.new.events_for_user(consultant[:github_account]).each do |event|
-        begin
-          persist_event(consultant, event)
-        rescue Moped::Errors::OperationFailure => e
-          unless duplicate_key_error?(e)
-            raise e
-          end
-          Rails.logger.info "duplicate event #{event[:event_id]} found for employee #{consultant[:employee_id]}"
-        end
+  class << self
+    def update_github consultants
+      consultants.each do |consultant|
+        update_github_for_consultant consultant
       end
     end
-  end
 
-  def self.github_events employee_id
-    GithubEvent.where(employee_id: employee_id).sort_by {|event| event[:created_at]}.reverse
-  end
+    def github_events employee_id
+      GithubEvent.where(employee_id: employee_id).sort_by {|event| event[:created_at]}.reverse
+    end
 
-  private
+    private
 
-  def self.persist_event(consultant, event)
-    GithubEvent.create(
-      employee_id: consultant[:employee_id],
-      event_id: event[:event_id],
-      type: event[:type],
-      repo_name: event[:repo_name],
-      languages: event[:languages],
-      created_at: event[:created_at],
-      avatar: event[:avatar])
-  end
+    def update_github_for_consultant(consultant)
+      GithubClient.new.events_for_user(consultant[:github_account]).each do |event|
+        persist_event consultant, event
+      end
+    end
 
-  def self.duplicate_key_error? e
-    e.details['code'] == 11000
+    def persist_event(consultant, event)
+      GithubEvent.create(
+        employee_id: consultant[:employee_id],
+        event_id: event[:event_id],
+        type: event[:type],
+        repo_name: event[:repo_name],
+        languages: event[:languages],
+        created_at: event[:created_at],
+        avatar: event[:avatar])
+    rescue Moped::Errors::OperationFailure => e
+      raise e unless duplicate_key_error?(e)
+      Rails.logger.info "duplicate event #{event[:event_id]} found for employee #{consultant[:employee_id]}"
+    end
+
+    def duplicate_key_error? e
+      e.details['code'] == 11000
+    end
   end
 
 end
