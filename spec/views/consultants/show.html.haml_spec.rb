c5d58d062a0e5bb0f470ffa634e5ff920a0df39d
Peiying Wen <wenpeiying@gmail.com>
2014-07-11 10:06:44 -0700

    fixed build

--- spec/views/consultants/show.html.haml_spec.rb
+++ spec/views/consultants/show.html.haml_spec.rb
@@ -21,7 +21,7 @@ describe 'consultants/show.html.haml' do
     @create_time = '2014-06-20 -0700'
     @relative_create_time = 'June 20, 2014 12:00am'
 
-    assign :activities, [GithubEvent.new(type: 'PushEvent', github_repository: GithubRepository.new(languages: {'Ruby'=> '1234'}, repo_name: 'repo'), created_at: @create_time, avatar: 'profile.png')]
+    assign :activities, [GithubEvent.new(type: 'PushEvent', github_repository: GithubRepository.new(languages: {'Ruby'=> '1234'}, repo_name: 'repo'), created_at: @create_time)]
 
     allow(connection_one).to receive(:teachable_skills).and_return(['Java'])
     allow(connection_one).to receive(:score).and_return(3.24)
@@ -92,8 +92,7 @@ describe 'consultants/show.html.haml' do
       expect(rendered).to have_text('repo')
       expect(rendered).to have_text(@relative_create_time)
       expect(rendered).to have_text('Ruby')
-      expect(rendered).to have_text('PushEvent')
-      expect(rendered).to have_css('div.avatar img')
+      expect(rendered).to have_text('Pushed')
     end
 
   end
