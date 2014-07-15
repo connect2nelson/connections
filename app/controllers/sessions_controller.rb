b903f9f89b9a94014fab59099f7c7625e0e90507
ThoughtWorker <foo@foo.com>
2014-07-15 13:41:40 -0700

    skipping authentication for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user	
 
   def create
     session[:auth_token] = request.env['omniauth.auth']
