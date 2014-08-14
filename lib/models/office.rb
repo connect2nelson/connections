36b26e8408757c7cac00c06c761cd90b528b7d26
Peiying Wen <wenpeiying@gmail.com>
2014-08-13 22:06:21 -0700

    return top skills of an office

--- lib/models/office.rb
+++ lib/models/office.rb
@@ -1,6 +1,19 @@
 class Office
   attr_reader :consultants
+
   def initialize(consultants)
     @consultants = consultants
+  end
+
+  def top_skills
+    consultants.inject({}) do |sum, consultant|
+      sum.merge(skills_for(consultant, sum))
+    end
+  end
+
+  private
+
+  def skills_for(consultant, aggregate={})
+    consultant.skills.inject(aggregate) {|sum, (k, v)| sum.merge(k => sum[k].to_i + v.to_i) }
   end
 end
