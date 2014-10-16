e030bcb98bcd12f095fd6e3e7aa36d92ca36d3eb
Sophie Krisch <skrisch@thoughtworks.com>
2014-10-16 14:46:25 -0700

    61 [Sophie] Styled the add sponsee section

--- app/controllers/sponsorship_controller.rb
+++ app/controllers/sponsorship_controller.rb
@@ -4,10 +4,10 @@ class SponsorshipController < ApplicationController
   autocomplete :consultant, :full_name, :full => true, :extra_data => [:employee_id]
 
   def create
-    if params[:sponsor_id].nil? && params[:sponsee_id].nil?
-      Sponsorship.create(sponsor_id: params[:sponsor_id], sponsee_id: params[:sponsee_id])
-    else
+    if params[:sponsor_id].nil? || params[:sponsee_id].nil?
       flash[:alert] = "I can't save a sponsee without an employee id!"
+    else
+      Sponsorship.create(sponsor_id: params[:sponsor_id], sponsee_id: params[:sponsee_id])
     end
 
     redirect_to '/consultants/'+ params[:sponsor_id]
