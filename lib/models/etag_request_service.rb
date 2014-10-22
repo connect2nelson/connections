7702f930857fcd1e3652869c8fdce9caf66b11de
Ian Norris <iannorris@inorris.local>
2014-10-22 11:42:04 -0700

    [Ian] adding error logging to make issue visible

--- lib/models/etag_request_service.rb
+++ lib/models/etag_request_service.rb
@@ -5,7 +5,9 @@ class EtagRequestService
     response = EtagRequest.new(url, etag).response
     ApiCallService.save url, response.headers[:etag]
     EtagResponse.new(response)
-  rescue
+  rescue => e 
+  	puts "An error occurred while trying to pull in github data"
+  	puts e
     return EtagNullResponse.new
   end
 
