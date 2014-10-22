039a3cfa037b74112d77c06302fb4f33571c8555
Ian Norris <iannorris@inorris.local>
2014-10-22 11:59:13 -0700

    [Ian] [54] sorting by consulting skills using a Set instead of a HashMap

--- lib/models/skill_type.rb
+++ lib/models/skill_type.rb
@@ -2,30 +2,30 @@ class SkillType
   attr_reader :tech_skills, :consulting_skills
 
   def initialize
-    @tech_skills = {'java' => 1, 'ruby' => 1, 'css' => 1, 'html' => 1, 'sql' => 1, 'mysql' => 1, 'cucumber' => 1, 'c#' => 1,
-                    'css3' => 1, 'ruby on rails' => 1, 'sass/scss' => 1, 'ios' => 1, 'go' => 1, 'scala' => 1, 'groovy' => 1,
-                    'capybara' => 1, 'php' => 1, 'html5' => 1, 'javascript' => 1, 'java application development' => 1,
-                    'javaScript application development' => 1, 'ui development' => 1, 'ruby application development' => 1,
-                    'c# application development' => 1, 'groovy application development' => 1}
+    @tech_skills = Set.new ['java', 'ruby', 'css' , 'html' , 'sql' , 'mysql' , 'cucumber' , 'c#',
+                    'css3' , 'ruby on rails' , 'sass/scss' , 'ios' , 'go' , 'scala' , 'groovy' ,
+                    'capybara' , 'php' , 'html5' , 'javascript' , 'java application development',
+                    'javaScript application development' , 'ui development' , 'ruby application development' ,
+                    'c# application development' , 'groovy application development' ]
 
-    @testing_skills = {'c# test automation' => 1, 'groovy test automation' => 1, 'java test automation' => 1, 'load testing' => 1,
-                      'manual/exploratory testing' => 1, 'mobile test automation - ios' => 1, 'performance testing' => 1,
-                      'selenium-webdriver' => 1, 'test parallelization (seleniumgrid, etc)' => 1}
+    @testing_skills = ['c# test automation' , 'groovy test automation' , 'java test automation' , 'load testing' ,
+                      'manual/exploratory testing' , 'mobile test automation - ios' , 'performance testing' ,
+                      'selenium-webdriver' , 'test parallelization (seleniumgrid, etc)' ]
 
-    @consulting_skills = {'problem solving' => 1, 'communication' => 1, 'teaching' => 1, 'presenting' => 1, 'questioning' => 1,
-                          'facilitation' => 1, 'influence' => 1, 'planning' => 1, 'negotiation' => 1, 'innovating' => 1,
-                          'system thinking' => 1, 'relationship building' => 1, 'establish & Cultivate Relationships' => 1,
-                          'requirements analysis' => 1, 'feedback & coaching' => 1, 'project planning & resourcing' => 1,
-                          'stakeholder management' => 1, 'iteration management' => 1, 'project management' => 1,
-                          'storytelling & storyboarding' => 1}
+    @consulting_skills = ['problem solving' , 'communication' , 'teaching' , 'presenting' , 'questioning' ,
+                          'facilitation' , 'influence' , 'planning' , 'negotiation' , 'innovating' ,
+                          'system thinking' , 'relationship building' , 'establish & Cultivate Relationships' ,
+                          'requirements analysis' , 'feedback & coaching' , 'project planning & resourcing' ,
+                          'stakeholder management' , 'iteration management' , 'project management' ,
+                          'storytelling & storyboarding' ]
   end
 
   def type_of(name)
-    if @tech_skills.has_key? name
+    if @tech_skills.include? name
       return 'tech'
-    elsif @testing_skills.has_key? name
+    elsif @testing_skills.include? name
       return 'testing'
-    elsif @consulting_skills.has_key? name
+    elsif @consulting_skills.include? name
       return 'consulting'
     else
       return ''
