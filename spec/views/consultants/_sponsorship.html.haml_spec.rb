1b728c7006e5ee74737ca394a7d25149343b79ad
Ian Norris <iannorris@inorris.local>
2014-10-23 14:13:49 -0700

    [61] [Ian] adding test to see button text

--- spec/views/consultants/_sponsorship.html.haml_spec.rb
+++ spec/views/consultants/_sponsorship.html.haml_spec.rb
@@ -3,11 +3,24 @@ require 'rails_helper'
 RSpec.describe "consultants/_sponsorship.html.haml", :type => :view do
 
   before do
+  	 a_consultant = Consultant.new(employee_id: '3', full_name: 'Ian Norris', primary_role: 'Dev', home_office: 'Chicago', working_office: 'San Francisco', skills: Hash['Ruby'=>'1', 'Cat'=>'5'])
+    mentee_one = Consultant.new(full_name: 'Charlie', employee_id: '4', skills: {})
+    mentee_two = Consultant.new(full_name: 'Dee', employee_id: '5', skills: {})
+
+    mentee_connection_one = Connection.new(a_consultant, mentee_one)
+    mentee_connection_two = Connection.new(a_consultant, mentee_two)
+    some_mentees = [mentee_connection_one, mentee_connection_two]
+
+    allow(mentee_connection_one).to receive(:teachable_skills).and_return(['Haskell'])
+    allow(mentee_connection_one).to receive(:score).and_return(4.10)
+    allow(mentee_connection_two).to receive(:teachable_skills).and_return(['Mandarin'])
+    allow(mentee_connection_two).to receive(:score).and_return(0.1333)
     stub_template "consultants/_mentees.html.haml" => ""
-    render
+    render "sponsorship", sponsees: some_mentees, consultant: a_consultant
   end
 
   describe 'show text box and button to add a sponsee' do
-    # specify{expect(rendered).to have_text('Add Sponsee')}
+  	specify{expect(rendered).to have_css "form[action='/sponsorship'][method='post']"}
+  	specify{expect(rendered).to have_css "input[value='Add Sponsee']"}
   end
 end
