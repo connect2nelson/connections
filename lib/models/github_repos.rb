2fecc258e7786b414c67ab8966fcf1ad43469c8c
Ian Norris <iannorris@inorris.local>
2014-10-23 10:11:25 -0700

    [54] [Ian] more renaming and refactoring to push out methods

--- lib/models/github_repos.rb
+++ lib/models/github_repos.rb
@@ -5,30 +5,6 @@ class GithubRepos
     @consultants = consultants
   end
 
-  def repo_groups_unordered
-    repo_groups_by_consultant_and_repo = {}
-    repo_groups_by_consultant_and_repo.default = 0
-
-    repo_groups = @consultants.map { |consultant| 
-      GithubEvent.where(:employee_id => consultant.employee_id).map { |event|
-        GithubActivity.new(event.github_repository.repo_name, consultant)
-        }
-      }.reduce(:+).reduce(repo_groups_by_consultant_and_repo) {|total, github_activity| 
-        total[github_activity] = total[github_activity] + 1
-        total
-      }
-      .reduce({}) {|total, (github_activity, occurrences)|
-        repo_name = github_activity.repo_name
-        consultant = github_activity.consultant
-        if total[repo_name]
-          total[repo_name][consultant]= occurrences
-        else
-          total[repo_name]= {consultant => occurrences}
-        end
-        total
-      }
-  end
-
   def repo_groups
     repo_groups_by_consultant_and_repo = {}
     repo_groups_by_consultant_and_repo.default = 0
@@ -46,6 +22,37 @@ class GithubRepos
     sorted_contributions = contributions.sort_by{ |consultant, number_of_contributions| number_of_contributions}.reverse
     sorted_contributions.flatten.delete_if{|element| element.is_a? Numeric}
   end
+
+  private
+
+  def grouped_by_consultant_and_repo_name
+    repo_groups_by_consultant_and_repo = {}
+    repo_groups_by_consultant_and_repo.default = 0
+    @consultants.map { |consultant| 
+      GithubEvent.where(:employee_id => consultant.employee_id).map { |event|
+        GithubActivity.new(event.github_repository.repo_name, consultant)
+        }
+      }.reduce(:+).reduce(repo_groups_by_consultant_and_repo) {|total, github_activity| 
+        total[github_activity] = total[github_activity] + 1
+        total
+      }
+  end
+
+  def repo_groups_unordered
+
+    repo_groups = grouped_by_consultant_and_repo_name
+      .reduce({}) {|total, (github_activity, occurrences)|
+        repo_name = github_activity.repo_name
+        consultant = github_activity.consultant
+        if total[repo_name]
+          total[repo_name][consultant]= occurrences
+        else
+          total[repo_name]= {consultant => occurrences}
+        end
+        total
+      }
+  end
+
 
 end
 
