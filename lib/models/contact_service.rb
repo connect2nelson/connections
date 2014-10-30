1858b0a749e51327e797dc48a4da5721a75ea30d
Peiying Wen <wenpeiying@gmail.com>
2014-10-29 17:15:52 -0700

    find github account name from consultant in contact service

--- lib/models/contact_service.rb
+++ lib/models/contact_service.rb
@@ -4,5 +4,11 @@ class ContactService
     Contact.where(:github_account.nin => ["", nil])
   end
 
+  def self.github_name_for consultant
+    contact = Contact.find_by(employee_id: consultant.employee_id)
+    return "" if contact.nil?
+    contact.github_account
+  end
+
 
 end
--- spec/lib/models/contact_service_spec.rb
+++ spec/lib/models/contact_service_spec.rb
@@ -13,4 +13,21 @@ describe ContactService do
     end
   end
 
+  context '.github_name_for' do
+
+    before do
+      Contact.create({employee_id: '111', github_account: 'yo'})
+    end
+
+    it 'should return empty string for an employee without github info' do
+      consultant = Consultant.new(employee_id: '000')
+      expect(ContactService.github_name_for consultant).to eq ""
+    end
+
+    it 'should return github account name for an employee with github info' do
+      consultant = Consultant.new(employee_id: '111')
+      expect(ContactService.github_name_for consultant).to eq 'yo'
+    end
+  end
+
 end
